e8aafb932ad5cc5c5981446c40536557477d487e
Joslyn-slp <55011603+Joslyn-slp@users.noreply.github.com>
2019-09-14 09:17:53 +0800

    Add files via upload

--- 代售/js/index.js
+++ 代售/js/index.js
@@ -1,4 +1,17 @@
 function wm(){
-	var fivedoor = document.getElementById("wm");
-	fivedoor.style.display = block;
+	var fivedoor = document.getElementById("sub-num");
+	fivedoor.style.display = "block";
+
+}
+function wmmove(){
+	var fivedoor = document.getElementById("sub-num");
+	fivedoor.style.display = "none";
+}
+function jm(){
+	var ninedoor = document.getElementById("sub-jm");
+	ninedoor.style.display = "block";
+}
+function jmmove(){
+	var ninedoor = document.getElementById("sub-jm");
+	ninedoor.style.display = "none";
 }
